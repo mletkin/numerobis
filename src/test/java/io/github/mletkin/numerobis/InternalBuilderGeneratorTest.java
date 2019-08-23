7b3dd22bf42a9c679f56203a85668b4e6bd233dd
Ullrich Rieger <vaporized@web.de>
2019-08-23 22:22:35 +0200

    added test for internal builder

---
+++ src/test/java/io/github/mletkin/numerobis/InternalBuilderGeneratorTest.java
@@ -0,0 +1,160 @@
+/**
+ * (c) 2019 by Ullrich Rieger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.mletkin.numerobis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+import io.github.mletkin.numerobis.generator.ClassUtil;
+import io.github.mletkin.numerobis.generator.Facade;
+import io.github.mletkin.numerobis.generator.GeneratorException;
+
+/**
+ * Builder generation without existing builder class.
+ */
+class InternalBuilderGeneratorTest {
+
+    @Test
+    void convertsClassWithField() {
+        assertThat(generateFromResource("TestClass")).isEqualTo(//
+                "public static class Builder {" //
+                        + "    private TestClass product;" //
+                        + "    public Builder() {" //
+                        + "        product = new TestClass();" //
+                        + "    }" //
+                        + "    public Builder withX(int x) {" //
+                        + "        product.x = x;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public TestClass build() {" //
+                        + "        return product;" //
+                        + "    }" //
+                        + "}");
+    }
+
+    @Test
+    void fieldWithAnnotationIsIgnored() {
+        assertThat(generateFromResource("TestClassIgnoreField")).isEqualTo(//
+                "public static class Builder {" //
+                        + "    private TestClassIgnoreField product;" //
+                        + "    public Builder() {" //
+                        + "        product = new TestClassIgnoreField();" //
+                        + "    }" //
+                        + "    public Builder withX(int x) {" //
+                        + "        product.x = x;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public TestClassIgnoreField build() {" //
+                        + "        return product;" //
+                        + "    }" //
+                        + "}");
+    }
+
+    @Test
+    void convertsClassWithTwoFields() {
+        assertThat(generateFromResource("TestClassTwo")).isEqualTo(//
+                "public static class Builder {" //
+                        + "    private TestClassTwo product;" //
+                        + "    public Builder() {" //
+                        + "        product = new TestClassTwo();" //
+                        + "    }" //
+                        + "    public Builder withX(int x) {" //
+                        + "        product.x = x;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public Builder withY(int y) {" //
+                        + "        product.y = y;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public TestClassTwo build() {" //
+                        + "        return product;" //
+                        + "    }" //
+                        + "}");
+    }
+
+    @Test
+    void addsWithMethodForPrivateField() {
+        assertThat(generateFromResource("TestClassWithPrivateField")).isEqualTo( //
+                "public static class Builder {" //
+                        + "    private TestClassWithPrivateField product;" //
+                        + "    public Builder() {" //
+                        + "        product = new TestClassWithPrivateField();" //
+                        + "    }" //
+                        + "    public Builder withX(int x) {" //
+                        + "        product.x = x;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public Builder withY(int y) {" //
+                        + "        product.y = y;" //
+                        + "        return this;" //
+                        + "    }" //
+                        + "    public TestClassWithPrivateField build() {" //
+                        + "        return product;" //
+                        + "    }" //
+                        + "}");
+    }
+
+    @Test
+    void notContainedClassProducesNothing() {
+        assertThatExceptionOfType(GeneratorException.class).isThrownBy( //
+                () -> Facade.withConstructors(StaticJavaParser.parseResource("TestClass.java"), "Foo",
+                        new CompilationUnit()))
+                .withMessage("Product class not found in compilation unit.");
+    }
+
+    @Test
+    void nullClassProducesNothing() {
+        assertThatExceptionOfType(GeneratorException.class).isThrownBy( //
+                () -> Facade.withConstructors(StaticJavaParser.parseResource("TestClass.java"), "",
+                        new CompilationUnit()))
+                .withMessage("Product class not found in compilation unit.");
+    }
+
+    @Disabled
+    @Test
+    void classWithoutUsableConstructorThrowsException() {
+        // The member class builder can use any constructor
+        // Although using a private constructor is not encouraged
+    }
+
+    private String generateFromResource(String className) {
+        try {
+            return extractBuilder(
+                    Facade.withConstructors(StaticJavaParser.parseResource(className + ".java"), className).productUnit,
+                    className).toString().replace("\r\n", "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ClassOrInterfaceDeclaration extractBuilder(CompilationUnit cu, String className) {
+        return ClassUtil.findClass(cu, className) //
+                .map(c -> ClassUtil.allMember(c, ClassOrInterfaceDeclaration.class)) //
+                .orElseGet(Stream::empty) //
+                .findFirst().get();
+    }
+
+}
