082a40a7d6012a5c60583b5003f9109b5e772cf1
mletkin <vaporized@web.de>
2019-09-07 20:15:22 +0200

    added missing NL and geader

--- src/main/java/io/github/mletkin/numerobis/plugin/Order.java
+++ src/main/java/io/github/mletkin/numerobis/plugin/Order.java
@@ -1,14 +1,35 @@
+/**
+ * (c) 2019 by Ullrich Rieger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.mletkin.numerobis.plugin;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.Optional;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 
 import io.github.mletkin.numerobis.generator.Facade;
 
+/**
+ * Class describing the processing of a single java file.
+ */
 class Order {
+
     private boolean generateBuilder = false;
     private boolean generateAccessors = false;
 
@@ -20,7 +41,7 @@ class Order {
 
     public Order(File productClassFile) {
         productPath = productClassFile.toPath();
-        product = PluginUtil.parse(productClassFile);
+        product = parse(productClassFile);
 
         generateBuilder = Facade.isBuilderWanted(product);
         generateAccessors = Facade.areAccessorsWanted(product);
@@ -29,11 +50,10 @@ class Order {
     void setBuilderPath(Path builderPath) {
         this.builderPath = builderPath;
         if (builderPath.toFile().exists()) {
-            builder = PluginUtil.parse(builderPath.toFile());
+            builder = parse(builderPath.toFile());
         } else {
             builder = new CompilationUnit();
         }
-        generateBuilder = true;
     }
 
     File productFile() {
@@ -70,5 +90,13 @@ class Order {
 
     boolean needsProcessing() {
         return generateAccessors || generateBuilder;
+    }
+
+    static CompilationUnit parse(File file) {
+        try {
+            return StaticJavaParser.parse(file);
+        } catch (FileNotFoundException e) {
+            throw new MojoFileNotFoundException(e);
+        }
     }
 }
